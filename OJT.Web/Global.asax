f75e1f31bdb24eeec0f34ee06abe3c4d797505e5
claudioherrera-4th <claudio.herrera@4thsource.com>
2014-09-22 15:57:41 -0500

    first commit

---
+++ OJT.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OJT.Web.MvcApplication" Language="C#" %>
---
+++ OJT.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OJT.Web.Startup))]
+namespace OJT.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
